c41f75709793dea2b854ed6f81f35ce6a8c9f2ae
Leocaliban <leocaliban@gmail.com>
2020-01-13 09:05:05 -0300

    Cria Heade para Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-import ContainerComponent from '../../components/ContainerComponent';
+import Container from '../../components/ContainerComponent';
 
-export default () => {
+import {
+    ConfigButtonArea,
+    ConfigButtonImage
+} from './HomeStyle';
+
+const Page = (props) => {
+
     return (
-        <ContainerComponent>
-            <Text>HOME WORKS</Text>
-        </ContainerComponent>
-    )
+        <Container>
+
+        </Container>
+    );
+};
+
+Page.navigationOptions = ({ navigation }) => {
+
+    const ConfigButton = () => {
+        const buttonAction = () => {
+            navigation.navigate('HomeConfig');
+        }
+
+        return (
+            <ConfigButtonArea onPress={buttonAction}>
+                <ConfigButtonImage source={require('../../assets/config.png')}></ConfigButtonImage>
+            </ConfigButtonArea>
+        )
+    }
+
+    return {
+        title: 'Seu progresso diário',
+        headerRight: <ConfigButton></ConfigButton>,
+        headerRightContainerStyle: {
+            marginRight: 10
+        }
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+    }
 }
+
+const mapDispatchProps = (dispatch) => {
+    return {
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchProps)(Page);
