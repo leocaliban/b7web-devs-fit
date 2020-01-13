60293357ca475b76b8ac023f084ff741cc2f0522
Leocaliban <leocaliban@gmail.com>
2020-01-13 09:05:05 -0300

    Cria Header para Home

---
+++ src/pages/Home/HomeStyle.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const ConfigButtonArea = styled.TouchableHighlight`
+    width: 30px;
+    height: 30px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ConfigButtonImage = styled.Image`
+    width: 25px;
+    height: 25px;
+`;
+
+
